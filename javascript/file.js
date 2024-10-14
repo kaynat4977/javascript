36ca844d185fa604b99a1ad6ee6a2a73ccf2c4a9
Kaynat Shabbir <shabbirkainat75@gmail.com>
2024-10-14 17:13:13 +0500

    Add files via upload

---
+++ javascript/file.js
@@ -0,0 +1,65 @@
+ //PRACTICE QUESTIONS//
+//let age = prompt("Enter your'e age !!");
+//switch (true) {
+ //   case (age >= 18 && age <=40):
+       // document.write("you are young enought !! Eligible");
+       // break;
+
+    //default:
+      //  document.write("you are not eligible");
+      //  break;
+//}
+//EXAMPLE
+//let name =("kainat");
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+///console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+//console.log(name);
+
+//PRACTICE QUESTIONS//
+//for(let i= 1 ; i<=50; i++){
+   // document.write( i ,"KAINAT SHABBIR <br>" );
+//}
+
+//let number = prompt("enter number");
+//if(num%2==0){
+   // document.write("even number");
+//}
+//else{
+  //  document.write("odd number");
+//}
+//for(let n=1 ; n<=20 ; n++){
+   // if(n%2==0){
+   // document.write(n,"even <br>");
+//}
+//else{
+  //  document.write(n,"odd <br>");
+//}
+//}
+
+//TABLE GENERATOR
+let table = prompt ("enter the table");
+for(let k=1; k<=10; k++){
+    document.write(table + "x" + k + "=" + table*k +"<br>");
+}
+
+
+
+
